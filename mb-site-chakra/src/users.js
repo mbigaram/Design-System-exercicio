b0032670a132380e52f5105d255b52e6c3c8527f
mbiga <mbigaram.mb@gmail.com>
2022-11-30 07:52:02 -0300

    Exercicios Design Systems Revisado e OK

--- mb-site-chakra/src/users.js
+++ mb-site-chakra/src/users.js
@@ -1,34 +1,54 @@
-const users = [
+
+
+
+
+
+
+
+import imagemStarship from "./assets/img/starship.png"
+import imagemBlue from "./assets/img/new-shepard.png"
+import imagemAriane from "./assets/img/ariane.png"
+
+
+import imagemNeutron from "./assets/img/neutron.png"
+
+
+
+export const users = [
       {
         id: "1",
         name: "Neutron",
         marca: "Rocket Lab",
-        precoDesconto: 100,
-        precoNormal: 120,
+        precoDesconto: 200,
+        precoNormal: 250,
+        imagem: imagemNeutron
         
       },
       {
         id: "2",
         name: "New Shepard",
-        marca: "Rocket Lab",
+        marca: "Blue Origin",
         precoDesconto: 100,
         precoNormal: 120,
+        imagem: imagemBlue
         
       },
       {
         id: "1",
-        name: "Blue Origin",
-        marca: "Rocket Lab",
-        precoDesconto: 100,
-        precoNormal: 120,
+        name: "Energia",
+        marca: "Roscosmos",
+        precoDesconto: 50,
+        precoNormal: 90,
+        imagem: imagemStarship
         
       },
       {
         id: "1",
         name: "Ariane 6",
-        marca: "Rocket Lab",
-        precoDesconto: 100,
-        precoNormal: 120,
+        marca: "Ariane Group",
+        precoDesconto: 90,
+        precoNormal: 110,
+        imagem: imagemAriane
         
       },
     ]
